bc3ae64521c2954857d46d207aaa1b1be8abb500
beebdoles <beebdoles132435@gmail.com>
2026-03-24 13:25:54 -0400

    finish later im lazy

--- main.c
+++ main.c
@@ -12,11 +12,34 @@
 
 int main(void);
 int countComponents(void);
-int createValidCombinations(void);
+int createValidCombinations(int* grid, int val, int index, int depth);
 
 
 int main(void) {
     
     printf("HI");
+
+    int* grid = malloc(10 * 10 * sizeof(int));
+
+    for (int i = 0; i < 10 * 10; ++i) {
+        if (i % 10 == 0) { printf("\n"); }
+        grid[i] = i;
+        printf("%d ", grid[i]);
+        if (i < 10) { printf(" "); }
+    }
+    printf("\n");
+
+    free(grid);
+
+    return 0;
+}
+
+int createValidCombinations(int* grid, int val, int index, int depth) {
+
+    if (val == 0) { return 0; }
+
+    grid[depth * 10 + index] = val;
+    
+
     return 0;
 }
