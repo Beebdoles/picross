cc5cae4874ae2faf9bb7c6ce241b2931beae6198
beebdoles <beebdoles132435@gmail.com>
2026-03-25 17:27:50 -0400

    finish later

--- Sequence.c
+++ Sequence.c
@@ -19,13 +19,30 @@ void createCombinations(Sequence* sq) {
     int sum = 0;
     int count = 0;
         
-    while (*sq->values != -1) {
+    while (*(sq->values + count) != -1) {
         
-        printf("%d ", *sq->values);
-
-        ++sq->values;
+        printf("%d ", *(sq->values + count));
         ++count;
+        sum += *(sq->values + count);
     }
     printf("\n");
-    sq->values -= count;
+
+    int freeBlocks = sq->size - (sum + count - 1);
+    int freeSpaces = count + 1;
+}
+
+void generateCombination(Sequence* sq, int value, int index, int depth, int freeSpaces) {i
+
+    if (depth == 0) {
+        
+        while (*(sq->combinations + depth) != NULL) { ++depth; }
+
+        *(sq->combinations + depth) = (int*)malloc(sizeof(int));
+    }
+    if (depth > freeSpaces) { (*(sq->combinations) + index) == -1; }
+    
+    for (int i = 0; i <= value; ++i) {
+
+        createCombinations(sq, value - i, ++index, depth);
+    }
 }
