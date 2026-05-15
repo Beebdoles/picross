787040ce91042b5ae84b0499ad275452d2894495
beebdoles <beebdoles132435@gmail.com>
2026-05-15 00:58:43 -0400

    rewrote parser, needs some cleanup work tho

--- main.c
+++ main.c
@@ -9,15 +9,29 @@ int main(void);
 int countComponents(void);
 int createValidCombinations(int* grid, int val, int index, int depth);
 void test(void);
-void parse(void);
 int run(void);
+int readInput(int* size, char* inputTopNums, char* inputBottomNums);
+int parse(int* size, int** topNums, int** bottomNums, char* inputTopNums, char* inputBottomNums);
 void freeMemory(Sequence* sq);
 
 int main(void) {
     
-    //test();
-    parse();
-    //run();
+    int size;
+    char* inputTopNums = (char*)malloc(sizeof(char) * 500);
+    char* inputBottomNums = (char*)malloc(sizeof(char) * 500);
+
+    if (readInput(&size, inputTopNums, inputBottomNums) == 1) { return 1; };            //i love memory leak
+
+    int** topNums = (int**)malloc(sizeof(int*) * size);
+    int** bottomNums = (int**)malloc(sizeof(int*) * size);
+
+    if (parse(&size, topNums, bottomNums, inputTopNums, inputBottomNums) == 1) { return 1; }                 //i love memory leak
+
+    //frees
+    for (int i = 0; i < size; ++i) { free(*(topNums + i)); free(*(bottomNums + i)); }
+
+    free(inputTopNums); free(inputBottomNums);
+    free(topNums); free(bottomNums);
 
     return 0;
 }
@@ -43,148 +57,126 @@ int run() {
     return 0;
 }
 
-void parse() {
-    
-    //accept inputs here
+int readInput(int* size, char* inputTopNums, char* inputBottomNums) {
 
     printf("Enter board size: ");
     char inputSize[100];
-    int size;
     fgets(inputSize, 100, stdin);
-    sscanf(inputSize, "%d", &size);
+    sscanf(inputSize, "%d", size);
 
-    int** topNums = (int**)malloc(sizeof(int*) * size);
-    int** bottomNums = (int**)malloc(sizeof(int*) * size);
-
-    for (int i = 0; i < size; ++i) { 
-
-        *(topNums + i) = (int*)malloc(sizeof(int) * (size / 2 + 1 + (size % 2)));
-        *(bottomNums + i) = (int*)malloc(sizeof(int) * (size / 2 + 1 + (size % 2)));
-    }
-
-    
     printf("Enter top numbers, delimited by '|': ");
-    char inputTopNums[500];
     fgets(inputTopNums, 500 - 1, stdin);
     
     printf("Enter bottom numbers, delimited by '|': ");
-    char inputBottomNums[500];
     fgets(inputBottomNums, 500 - 1, stdin);
 
-    //parse inputs here
+    return 0;
+}
+
+int parse(int* size, int** topNums, int** bottomNums, char* inputTopNums, char* inputBottomNums) {
+
+    for (int i = 0; i < *size; ++i) { 
+
+        *(topNums + i) = (int*)malloc(sizeof(int) * (*size / 2 + 1 + (*size % 2)));
+        *(bottomNums + i) = (int*)malloc(sizeof(int) * (*size / 2 + 1 + (*size % 2)));
+    }
 
     int section = 0, valIndex = 0, length = 0;
-    char* value = (char*)malloc(sizeof(char) * 4); memset(value, '\0', 4);
+    char* value = (char*)malloc(sizeof(char) * 4); memset(value, '\0', 4);              //read 3 digit val at most
     int prevWhitespace = False;
 
-    for (int i = 0; inputTopNums[i] != '\0' && i < 500; ++i) {
+    for (int i = 0; *(inputTopNums + i) != '\0' && i < 500; ++i) {
+   
+        if (valIndex > (*size / 2 + 1 + (*size % 2))) { printf("Too many blocks in section\n"); return 1; }
+        if (length > 3) { printf("Digit too large ( > 3)\n"); return 1; }
 
-        //if whitespace
-        if (inputTopNums[i] == 32 && !prevWhitespace) {
+        //write a value if whitespace or semicolon
+        if ((*(inputTopNums + i) == 32 && prevWhitespace == False) || *(inputTopNums + i) == 59 || *(inputTopNums + i) == 10) {
         
             value -= length;
-            *(*(topNums + section) + valIndex) = atoi(value);
-            memset(value, '\0', length); length = 0;
+            *(*(topNums + section) + valIndex) = atoi(value);                           //safety write val + sentinel
+            *(*(topNums + section) + valIndex + 1) = -1;                    
+            memset(value, '\0', 4); length = 0;
             ++valIndex;
             prevWhitespace = True;
-        }
 
-        //if pipe
-        else if (inputTopNums[i] == 124) {
-        
-            *(*(topNums + section) + valIndex) = -1;
-            valIndex = 0;
-            value -= length; memset(value, '\0', length);
-            prevWhitespace = True;
-            ++section;
-            length = 0;
-        }
-
-        //if normal
-        else if (inputTopNums[i] >= 48 && inputTopNums[i] <= 57) {
+            if (*(inputTopNums + i) == 59) {
             
-            *value = inputTopNums[i];
+                ++section;
+                valIndex = 0;
+            }
+
+            if (*(inputTopNums + i) == 10) {
+            
+                ++section;
+                break;
+            }
+        }
+
+        //read a value
+        else if (*(inputTopNums + i) >= 48 && *(inputTopNums + i) <= 57) {
+            
+            *value = *(inputTopNums + i);
             ++value;
             ++length;
             prevWhitespace = False;
         }
-
-        //newline, combine section + newval
-        else if (inputTopNums[i] == 10) {
-        
-            value -= length;
-            *(*(topNums + section) + valIndex) = atoi(value);
-            memset(value, '\0', length);
-            length = 0;
-            ++valIndex;
-            
-            *(*(topNums + section) + valIndex) = -1;
-            valIndex = 0;
-            prevWhitespace = True;
-            ++section;
-        }
     }
+
+    if (section > *size) { printf("Too many sections\n"); return 1; };
 
     memset(value, '\0', 4);
     section = 0; valIndex = 0; length = 0;
     prevWhitespace = False;
 
-    for (int i = 0; inputBottomNums[i] != '\0' && i < 500; ++i) {
+    for (int i = 0; *(inputBottomNums + i) != '\0' && i < 500; ++i) {
+    
+        if (valIndex > (*size / 2 + 1 + (*size % 2))) { printf("Too many blocks in section\n"); return 1; }
+        if (length > 3) { printf("Digit too large ( > 3)\n"); return 1; }
 
-        //if whitespace
-        if (inputBottomNums[i] == 32 && !prevWhitespace) {
+        //write a value if whitespace or semicolon
+        if ((*(inputBottomNums + i) == 32 && prevWhitespace == False) || *(inputBottomNums + i) == 59 || *(inputBottomNums + i) == 10) {
         
             value -= length;
-            *(*(bottomNums + section) + valIndex) = atoi(value);
-            memset(value, '\0', length); length = 0;
+            *(*(bottomNums + section) + valIndex) = atoi(value);                           //safety write val + sentinel
+            *(*(bottomNums + section) + valIndex + 1) = -1;                    
+            memset(value, '\0', 4); length = 0;
             ++valIndex;
             prevWhitespace = True;
-        }
 
-        //if pipe
-        else if (inputBottomNums[i] == 124) {
-        
-            *(*(bottomNums + section) + valIndex) = -1;
-            valIndex = 0;
-            value -= length; memset(value, '\0', length);
-            prevWhitespace = True;
-            ++section;
-            length = 0;
-        }
-
-        //if normal
-        else if (inputBottomNums[i] >= 48 && inputBottomNums[i] <= 57) {
+            if (*(inputBottomNums + i) == 59) {
             
-            *value = inputBottomNums[i];
+                ++section;
+                valIndex = 0;
+            }
+
+            if (*(inputBottomNums + i) == 10) {
+            
+                ++section;
+                break;
+            }
+        }
+
+        //read a value
+        else if (*(inputBottomNums + i) >= 48 && *(inputBottomNums + i) <= 57) {
+            
+            *value = *(inputBottomNums + i);
             ++value;
             ++length;
             prevWhitespace = False;
         }
-
-        //newline, combine section + newval
-        else if (inputBottomNums[i] == 10) {
-        
-            value -= length;
-            *(*(bottomNums + section) + valIndex) = atoi(value);
-            memset(value, '\0', length);
-            length = 0;
-            ++valIndex;
-            
-            *(*(bottomNums + section) + valIndex) = -1;
-            valIndex = 0;
-            prevWhitespace = True;
-            ++section;
-        }
     }
+
+    if (section > *size) { printf("Too many sections\n"); return 1; };
 
     printf("\n");
     printf("Tops:\n");
 
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < *size; ++i) {
     
         int j = 0;
         
-        while (*(*(topNums + i) + j) != -1 && j < size) {
+        while (*(*(topNums + i) + j) != -1 && j < *size) {
         
             printf("%d ", *(*(topNums + i) + j));
             ++j;
@@ -195,11 +187,11 @@ void parse() {
 
     printf("\nBottoms:\n");
 
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < *size; ++i) {
     
         int j = 0;
 
-        while (*(*(bottomNums + i) + j) != -1 && j < size) {
+        while (*(*(bottomNums + i) + j) != -1 && j < *size) {
         
             printf("%d ", *(*(bottomNums + i) + j));
             ++j;
@@ -208,12 +200,9 @@ void parse() {
         printf("\n");
     }
 
-    //free
-    
     free(value);
-    for (int i = 0; i < size; ++i) { free(*(topNums + i)); free(*(bottomNums + i)); }
-    free(topNums); free(bottomNums);
 
+    return 0;
 }
 
 void test() {
